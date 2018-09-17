d53cd611c3edd2e8c73a97304a6a54bc57a36359
OYQ <aquan1264@vip.qq.com>
2018-09-17 17:19:57 +0800

    init

---
+++ OYNetSpeedTool/OYNetSpeedTool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OYNetSpeedTool
+//
+//  Created by 欧阳铨 on 2018/9/17.
+//  Copyright © 2018年 欧阳铨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
